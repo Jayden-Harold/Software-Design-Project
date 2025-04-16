a098b78ccbbf3198dd46eb8e3367dba13505255b
TariroMuvevi <2667536@students.wits.ac.za>
2025-04-16 12:17:36 +0200

    Update info.js

--- public/info.js
+++ public/info.js
@@ -2,6 +2,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebas
    import { getAuth, GoogleAuthProvider, signInWithCredential, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
    import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
+   const firebaseConfig = {
+    apiKey: "AIzaSyDSqHKGzYj8bUzKGoFHH93x3Wlq4G463yY",
+    authDomain: "greensmoke-ee894.firebaseapp.com",
+    projectId: "greensmoke-ee894",
+    storageBucket: "greensmoke-ee894.firebasestorage.app",
+    messagingSenderId: "140065144019",
+    appId: "1:140065144019:web:48e4963e4826a85aca2826"
+  };
+  
+    const app = initializeApp(firebaseConfig);
+      const auth = getAuth(app);
+      const db = getFirestore(app);
+
    async function updateUserProfile(user) {
     try {
         const userRef = doc(db, "users", user.uid);
