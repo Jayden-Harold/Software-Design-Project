9a8e5918adb58923ef4f0d4ff24d5b1837d290e5
Jayden-Harold <2656236@students.wits.ac.za>
2025-04-17 22:57:34 +0200

    Frontend Styling

---
+++ public/admin.js
@@ -0,0 +1,13 @@
+const residents = document.querySelector("#residents-img");
+const staff = document.querySelector("#staff-img");
+const bookings = document.querySelector("#bookings-img");
+
+residents.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "admin_resident.html";
+})
+
+staff.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "admin_staff.html";
+})
