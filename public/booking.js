22eb612a5a95ac0d0b0e61879ed955b4fbe988d3
DylanNaidoo <2667989@students.wits.ac.za>
2025-04-21 17:50:29 +0200

    Update booking.js

--- public/booking.js
+++ public/booking.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+   import { getAuth, GoogleAuthProvider, signInWithCredential, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+   import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+   const firebaseConfig = {
+    apiKey: "AIzaSyDSqHKGzYj8bUzKGoFHH93x3Wlq4G463yY",
+    authDomain: "greensmoke-ee894.firebaseapp.com",
+    projectId: "greensmoke-ee894",
+    storageBucket: "greensmoke-ee894.firebasestorage.app",
+    messagingSenderId: "140065144019",
+    appId: "1:140065144019:web:48e4963e4826a85aca2826"
+  };
+  
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+    const user = auth.currentUser;
+
+
+
 const book_btn = document.querySelector("#book-btn");
 const modal_booking = document.querySelector(".modal-booking")
 
