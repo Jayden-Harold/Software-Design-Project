b119aedeab5bb95ad30d77155d0c04e52fc1b4a9
DylanNaidoo <2667989@students.wits.ac.za>
2025-04-30 11:45:49 +0200

    Create admin_maintenance.js

---
+++ public/admin_maintenance.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+   import { getAuth, GoogleAuthProvider, signInWithCredential, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+   import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+   import { collection, query, where, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+   const firebaseConfig = {
+    apiKey: "AIzaSyDSqHKGzYj8bUzKGoFHH93x3Wlq4G463yY",
+    authDomain: "greensmoke-ee894.firebaseapp.com",
+    projectId: "greensmoke-ee894",
+    storageBucket: "greensmoke-ee894.firebasestorage.app",
+    messagingSenderId: "140065144019",
+    appId: "1:140065144019:web:48e4963e4826a85aca2826"
+  };
+
+  const menu = document.querySelector("#mobile-menu");
+  const menuLinks = document.querySelector(".nav-menu");
+  
+  // Toggle Mobile Menu
+  menu.addEventListener("click", () => {
+      menu.classList.toggle("is-active");
+      menuLinks.classList.toggle("active");
+  });
+  
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+    const user = auth.currentUser;
