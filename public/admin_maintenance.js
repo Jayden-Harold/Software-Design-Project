194eb1996d34877a4756aca9591aef5f21f0c9a0
Jayden-Harold <2656236@students.wits.ac.za>
2025-05-02 10:56:08 +0200

    Admin Maintenance Table

--- public/admin_maintenance.js
+++ public/admin_maintenance.js
@@ -25,3 +25,63 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebas
     const auth = getAuth(app);
     const db = getFirestore(app);
     const user = auth.currentUser;
+
+  const mainTableBody = document.querySelector("#mainTable").getElementsByTagName("tbody")[0];
+
+  async function DisplayReports() {
+    try {
+        mainTableBody.innerHTML = ""; // Clear existing rows
+
+        const mainRef = collection(db, "Maintenance");
+        const q = query(mainRef);
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach((docSnap) => {
+            const mainData = docSnap.data();
+
+            const tr = document.createElement("tr");
+            const facTd = document.createElement("td");
+            const catTd = document.createElement("td");
+            const descTd = document.createElement("td");
+            const assignedTd = document.createElement("td");
+            const statTd = document.createElement("td");
+            const dateTd = document.createElement("td");
+
+            const createdAt = mainData.ReportedDate?.toDate ? mainData.ReportedDate.toDate() : new Date();
+            dateTd.textContent = createdAt.toLocaleString();
+
+            facTd.textContent = mainData.facility;
+            catTd.textContent = mainData.category;
+            descTd.textContent = mainData.description;
+            statTd.textContent = mainData.Status;
+            const select = document.createElement('select');
+            ['Pending', 'Approved', 'Denied'].forEach(status => {
+                const option = document.createElement('option');
+                option.value = status.toLowerCase();
+                option.textContent = status;
+                select.appendChild(option);
+            });
+
+            tr.appendChild(facTd);
+            tr.appendChild(catTd);
+            tr.appendChild(descTd);
+            tr.appendChild(select);
+            tr.appendChild(dateTd);
+            tr.appendChild(statTd);
+
+            mainTableBody.appendChild(tr);
+
+        });
+    } catch (error) {
+        console.error("Error fetching bookings:", error);
+    }
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        DisplayReports();
+    } else {
+        alert("User not signed in. Redirecting...");
+        window.location.href = "index.html";
+    }
+});
